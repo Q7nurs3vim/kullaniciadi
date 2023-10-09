a754ab0f6853fa2b86908dd2fc08d84e6f1ba651
Luz <145450869+Q7nurs3vim@users.noreply.github.com>
2023-10-09 12:20:37 +0300

    Add files via upload

---
+++ uygulama1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace uygulama1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGir
+            // 
+            this.btnGir.Location = new System.Drawing.Point(37, 71);
+            this.btnGir.Name = "btnGir";
+            this.btnGir.Size = new System.Drawing.Size(75, 23);
+            this.btnGir.TabIndex = 0;
+            this.btnGir.Text = "Gir";
+            this.btnGir.UseVisualStyleBackColor = true;
+            this.btnGir.Click += new System.EventHandler(this.btnGir_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(148, 116);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGir;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ uygulama1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGir_Click(object sender, EventArgs e)
+        {
+            string kullanici_adi;
+            kullanici_adi = textBox1.Text;
+            if (kullanici_adi == "milliegitim @meb.k12.tr")
+            {
+                MessageBox.Show("Kullanıcı sisteme kayıtlıdır");
+            }
+             else
+            {
+                MessageBox.Show("Kullanıcı adınız yanlıştır");
+            }
+        }
+    }
+}
